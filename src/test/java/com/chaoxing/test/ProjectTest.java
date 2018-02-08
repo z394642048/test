580e87db350ead4fd8892a26f4d198fe1736bd70
陈敏 <z394642048@163.com>
2018-02-08 17:36:13 +0800

    xxx

---
+++ src/test/java/com/chaoxing/test/ProjectTest.java
@@ -0,0 +1,31 @@
+package com.chaoxing.test;
+
+import com.chaoxing.test.model.Menu;
+import com.chaoxing.test.service.IFanyaUserDetailService;
+import com.chaoxing.test.service.IMenuService;
+import com.chaoxing.test.service.IOpenAcaService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectTest {
+
+
+    @Autowired
+    private IMenuService menuService;
+
+
+    @Test
+    public void index() {
+
+        List<Menu> menuByUid = menuService.getMenuByUid((long) 1);
+        System.out.println(menuByUid);
+
+    }
+}
