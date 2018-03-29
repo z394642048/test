6917c45ac2892f6738ea61d52c126843e9419cf0
陈敏 <chenmin@chaoxing.com>
2018-03-29 12:00:54 +0800

    七牛云上传

---
+++ src/test/java/com/chaoxing/test/QiNiuTest.java
@@ -0,0 +1,54 @@
+package com.chaoxing.test;
+
+import com.google.gson.Gson;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
+import com.qiniu.util.Auth;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QiNiuTest {
+
+    @Test
+    public void test1() {
+        //构造一个带指定Zone对象的配置类
+        Configuration cfg = new Configuration(Zone.zone2());
+        //...其他参数参考类注释
+        UploadManager uploadManager = new UploadManager(cfg);
+        //...生成上传凭证，然后准备上传
+        String accessKey = "EN9A5uCDBEK6RhJJJMRtNcqAOtktxWFEO6oC8ftc";
+        String secretKey = "XabWVs1pvjSXGRjlvWUIxP1_12S6crUW0W3QPWdM";
+        String bucket = "bucket";
+        //如果是Windows情况下，格式是 D:\\qiniu\\test.png
+        String localFilePath = "C:\\Users\\Public\\Pictures\\Sample Pictures\\20180108170925222.png";
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
+        String key = null;
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket);
+
+        try {
+            Response response = uploadManager.put(localFilePath, key, upToken);
+            //解析上传成功的结果
+            DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+            //该文件在七牛云中的名字
+            System.out.println(putRet.key);
+            System.out.println(putRet.hash);
+        } catch (QiniuException ex) {
+            Response r = ex.response;
+            System.err.println(r.toString());
+            try {
+                System.err.println(r.bodyString());
+            } catch (QiniuException ex2) {
+                //ignore
+            }
+        }
+    }
+}
