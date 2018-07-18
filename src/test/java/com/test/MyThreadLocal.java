c7d91f81c290ad9640811bb4dfee937e7399b279
陈敏 <chenmin@chaoxing.com>
2018-07-18 18:08:29 +0800

    ThreadLocal

--- src/test/java/com/test/MyThreadLocal.java
+++ src/test/java/com/test/MyThreadLocal.java
@@ -2,6 +2,8 @@ package com.test;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,12 +18,41 @@ public class MyThreadLocal {
                 @Override
                 public void run() {
                     local.set( "值：" + Thread.currentThread().getName() );
+                    Object o1 = local.get();
+                    local.set( "值：" + Thread.currentThread().getId() );
+                    Object o2 = local.get();
                 }
             } );
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println( local.get() );
-        }
+    }
 
+    @Test
+    public void test2() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool( 10 );
+        ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        final int[] j = {0};
+        for (int i = 0; i < 100000; i++) {
+            executorService.execute( new Runnable() {
+                @Override
+                public void run() {
+                    int i1 = j[0]++;
+                    map.put( "x",i1 );
+                }
+            } );
+        }
+        final int[] k = {0};
+        for (int i = 0; i < 100000; i++) {
+            executorService.execute( new Runnable() {
+                @Override
+                public void run() {
+                    int i1 = k[0]++;
+                    hashMap.put( "y",i1 );
+                }
+            } );
+        }
+        Thread.sleep( 3000 );
+        System.out.println(map);
+        System.out.println(hashMap);
     }
 }
