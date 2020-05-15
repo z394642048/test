85213ff17ae0a4d4c0885c6bc73f4dad02b8f4f0
chenmin <z394642048@163.com>
2020-05-15 11:27:16 +0800

    算法练习 一个公交车经过n个站点，乘客从前门上车，从后门下车。      现在统计了在第i个站，下车人数a[i]，以及上车人数b[i]。      问公交车运行时候车上最多有多少乘客

--- src/test/java/com/test/Main.java
+++ src/test/java/com/test/Main.java
@@ -1,24 +1,39 @@
 package com.test;
 
-import java.util.Scanner;
+import org.junit.Test;
+
+import java.util.*;
 
 /**
- * 算法测试题
+ * 算法练习题
  */
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int s = scanner.nextInt();
-        int d = s - Math.abs(a) - Math.abs(b);
-        if (d >= 0 && d % 2 == 0) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
+        int sum = 0;
+        int max = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += b - a;
+            if (max < sum) {
+                max = sum;
+            }
         }
-        scanner.close();
+        System.out.println(max);
     }
 
+    @Test
+    public void test2() {
+        int sum1 = 1;
+        int sum2 = 1;
+        int b = 5;
+        int a = 8;
+        System.out.println(sum1 += b);
+        System.out.println(sum2 += -a + b);
+        System.out.println(sum1);
+        System.out.println(sum2);
+    }
 }
